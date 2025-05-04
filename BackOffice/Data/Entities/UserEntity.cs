9468e5abb829e8396ef0bad7f8813b0bb29bd131
Choco <mayanguyen2005@gmail.com>
2025-05-04 23:49:09 +0200

    Add files via upload

---
+++ BackOffice/Data/Entities/UserEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities;
+
+public class UserEntity : IdentityUser
+{
+    public string? FullName { get; set; }
+    public string? JobTitle { get; set; }
+
+    public virtual ICollection<ProjectEntity> Projects { get; set; } = [];
+}
